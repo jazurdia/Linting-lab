4df27459665cd265be61665757a9c2bc5b3a6ac1
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2023-03-07 18:37:08 -0600

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import ducklogo from './assets/ducklogo.svg'
 import './App.css'
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={ducklogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Hello PANAAAAAS</h1>
