05880f813be54df0e523fe5024c23f92214fadb6
Mark Albrand <mark.albrand5@gmail.com>
2023-03-09 00:03:26 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,11 @@ function App() {
 
   return (
     <div className="App">
-    <div>
-    <img src={duckLogo} alt="react logo" />
-    </div>
-    <Textdesc />
-    <CheckItem></CheckItem>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div>
+            <img src={duckLogo} alt="react logo" />
+        </div>
+        <Textdesc />
+        <CheckItem />
     </div>
   )
 }
