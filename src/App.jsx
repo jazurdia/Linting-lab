0177a0e575097891f4f859c8b75bcc185e7d181d
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2023-03-09 19:46:45 -0600

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import duckLogo from './Logo.jsx'
 import './Textdesc.jsx'
 import './App.css'
 import './CheckItem.css'
-import { Textdesc } from './Textdesc.jsx'
+import { Textdesc, Textdescparagraph } from './Textdesc.jsx'
 import { CheckItem } from './CheckItem'
 import {InfoCards} from "./InfoCards.jsx";
 import { Button } from './Button.jsx'
@@ -17,18 +17,23 @@ function App() {
         <div className="logo">
             <img src={duckLogo} alt="DuckDuckGo logo" className="main_logo"/>
         </div>
-        <div className='textdesc'>
-            <h2>¿Cansado de que te rastreen? Podemos ayudarte.</h2>
-            <p>Consigue protección continua y gratuita para tu privacidad en el navegador con una descarga:</p>
-        </div>
+        <Textdesc className='textdesc'>
+            ¿Cansado de que te rastreen? Podemos ayudarte.
+        </Textdesc>
+        <Textdescparagraph className='textdesc'> 
+          No almacenamos información personal. No te persegimos con anuncios. No te rastreamos nunca.
+        </Textdescparagraph>
         <Input/>
         <CheckItem />
         <Button className="blue" > Añadir DuckDuckGo a Chrome</Button>
         <Button className="green">Instalar DuckDuckGo </Button>
         <Textdesc className='textdesc'>
-            <h2>No guardamos tu información <br></br> personal. Y nunca lo haremos.</h2>
-            <p>Nuestra política de privacidad es simple: No colectamos <br></br> o compartimos ninguna información personal tuya.</p>
+            No guardamos tu información <br></br> personal. Y nunca lo haremos. 
         </Textdesc>
+        <Textdescparagraph className='textdesc'>
+        Nuestra política de privacidad es simple: No colectamos <br></br> o compartimos ninguna información personal tuya.
+        </Textdescparagraph>
+
         <InfoCards/>
     </div>
   )
