aee460ada4d5f94085ade4428678a033df4ff0b9
Angel Castellanos <angel@pop-os.localdomain>
2023-03-09 20:28:19 -0600

    barra de busqueda final v1

--- src/Input.jsx
+++ src/Input.jsx
@@ -5,21 +5,21 @@ const Input = () => {
   return (
     <div>
         <form className="search_form">
-        <input
-            className = "search_input"
-            placeholder="Busca en la red sin que te rastreen"
-            type="text"
-            tabIndex={1}
-        >
-        </input>
-        <input
-            className = "search_button"
-            type="submit"
-            tabIndex={2}
-        >
-        </input>
+            <input
+                className = "search_input"
+                placeholder="Busca en la red sin que te rastreen"
+                type="text"
+                tabIndex={1}
+            >
+            </input>
+            <input
+                className = "search_button"
+                type="submit"
+                tabIndex={2}
+            >
+            </input>
 
-    </form>
+        </form>
     </div>
     
   );
