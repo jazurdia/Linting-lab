d7777e43e6167e7fb8bc43d586a84edd911f5e9f
Mark Albrand <mark.albrand5@gmail.com>
2023-03-09 19:40:02 -0600

    Cards

--- src/InfoCards.jsx
+++ src/InfoCards.jsx
@@ -1,3 +1,5 @@
+import './InfoCards.css'
+
 const info_text= {
     card_1_info: 'Navega como siempre y nosotros nos encargamos del resto. Te ofrecemos buscador, bloqueador de rastreadores y mejora de encriptación todo en una extensión de Chrome',
     card_2_info: 'Busca de forma privada con nuestra app o extensión, añade búsqueda web privada a tu navegador preferido o busca en duckduckgo.com.',
@@ -8,9 +10,19 @@ const InfoCards = () => {
     return (
         <div className="info_cards_cont">
             <div className="info_card">
-                <img src="./assets/laptop.svg" alt="Laptop"/>
+                <img src="src/assets/laptop.svg" alt="Laptop" className="img_card"/>
                 <div className="card_title">Privacidad para Chrome</div>
                 <div className="card_info">{info_text.card_1_info}</div>
+            </div>
+            <div className="info_card">
+                <img src="src/assets/search.svg" alt="Laptop" className="img_card"/>
+                <div className="card_title">Motor de Búsqueda Privada</div>
+                <div className="card_info">{info_text.card_2_info}</div>
+            </div>
+            <div className="info_card">
+                <img src="src/assets/mobile.svg" alt="Laptop" className="img_card"/>
+                <div className="card_title">Navegador Privado</div>
+                <div className="card_info">{info_text.card_3_info}</div>
             </div>
         </div>
     )
