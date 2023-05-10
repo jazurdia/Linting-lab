45414ac32a8094ea2e4cb33d4e9052d92be40a4b
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2023-05-10 01:45:25 -0600

    Revert "implementación de eslint y prettier"
    This reverts commit abcc76feb98b4b94f929155ac40d33386df6b3a1.

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,11 +1,12 @@
-import "./Button.css";
+import "./Button.css"
 
-const Button = (props) => {
-  return (
-    <div>
-      <button className={props.className}>{props.children}</button>
-    </div>
-  );
-};
+const Button = (props) => 
+{
+    return (
+        <div>
+            <button className={props.className}>{props.children}</button>
+        </div>
+    )
+}
 
-export { Button };
+export { Button }
