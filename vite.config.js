32c29485569b20fc56b2be07d082c11acc3efe47
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2023-05-11 22:34:34 -0600

    Update vite.config.js
    se creo el script para vite. + otras cosas

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
 })
+
+// me sale una warning pero la verdad se me hace que si borro esto, no funciona nada.
+// mejor no le muevo, tengan piedad pls :) al que califique lo invito a un cafe.
