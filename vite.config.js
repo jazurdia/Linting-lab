6173fb7732b3d0318ffb60c7fe5f5cfe19cdb13e
Javier Alejandro Azurdia Arrecis <aleazurdia1@gmail.com>
2023-05-24 20:01:47 -0600

    linting revisado

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "",
 })
 
 // me sale una warning pero la verdad se me hace que si borro esto, no funciona nada.
